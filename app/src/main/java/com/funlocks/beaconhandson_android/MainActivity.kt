c93bf846102a8121f046ed586bfa5046065212ea
Kouta Takamatsu <bb1016181@gmail.com>
2020-10-28 22:53:55 +0900

    プロジェクト作成

---
+++ app/src/main/java/com/funlocks/beaconhandson_android/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.funlocks.beaconhandson_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
